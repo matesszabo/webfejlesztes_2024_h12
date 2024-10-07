b76d3fafb83961ce0f93ecbc40c62c78a6833fc4
matesszabo <matesszabo@hotmail.com>
2024-10-07 13:25:19 +0200

    project init

---
+++ src/main/java/hu/unideb/inf/rendeles/RendelesApplication.java
@@ -0,0 +1,13 @@
+package hu.unideb.inf.rendeles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RendelesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RendelesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/unideb/inf/rendeles/RendelesApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.unideb.inf.rendeles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RendelesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
