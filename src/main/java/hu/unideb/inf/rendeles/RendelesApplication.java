27b40c7ff0891bb299b8649c1dc670ffbd22f67a
matesszabo <matesszabo@hotmail.com>
2024-10-14 12:26:27 +0200

    helyentity

--- src/main/java/hu/unideb/inf/rendeles/RendelesApplication.java
+++ src/main/java/hu/unideb/inf/rendeles/RendelesApplication.java
@@ -2,8 +2,9 @@ package hu.unideb.inf.rendeles;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class RendelesApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/hu/unideb/inf/rendeles/controller/HelyEntity.java
@@ -0,0 +1,68 @@
+package hu.unideb.inf.rendeles.controller;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "hely")
+public class HelyEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    @Column(name = "sor", nullable = false)
+    private int sor;
+    @Column(name = "polc", nullable = false)
+    private int polc;
+
+    public HelyEntity() {
+    }
+
+    public HelyEntity(long id, int sor, int polc) {
+        this.id = id;
+        this.sor = sor;
+        this.polc = polc;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public int getSor() {
+        return sor;
+    }
+
+    public void setSor(int sor) {
+        this.sor = sor;
+    }
+
+    public int getPolc() {
+        return polc;
+    }
+
+    public void setPolc(int polc) {
+        this.polc = polc;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        HelyEntity that = (HelyEntity) o;
+        return id == that.id && sor == that.sor && polc == that.polc;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, sor, polc);
+    }
+}
+
+
+
+
