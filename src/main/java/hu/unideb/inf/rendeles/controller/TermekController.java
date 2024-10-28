4177542da50e1a4731f2a612ef77b181976e1a60
matesszabo <matesszabo@hotmail.com>
2024-10-28 13:24:16 +0100

    termek filter, read

--- src/main/java/hu/unideb/inf/rendeles/controller/TermekController.java
+++ src/main/java/hu/unideb/inf/rendeles/controller/TermekController.java
@@ -5,6 +5,9 @@ import hu.unideb.inf.rendeles.data.repository.TermekRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequestMapping("/api")
 public class TermekController {
@@ -30,4 +33,44 @@ public class TermekController {
     public void deleteTermek(@RequestParam Long id){
         repository.deleteById(id);
     }
+
+    @GetMapping("/termek")
+    public List<TermekEntity> findAllTermek(){
+        return repository.findAll();
+    }
+
+    // /localhost:8080/api/termek/Doboz
+    @GetMapping("/termek/{nev}")
+    public List<TermekEntity> findAllByNev(@PathVariable String nev){
+        List<TermekEntity> valogatott = new ArrayList<TermekEntity>();
+        valogatott = repository.findAll()
+                .stream()
+                .filter(x -> x.getNev().equals(nev))
+                .toList();
+
+        return valogatott;
+    }
+
+    // /localhost:8080/api/termekbynev?nev=Doboz
+    @GetMapping("/termekbynev")
+    public List<TermekEntity> findByNev(@RequestParam String nev){
+        return repository.findAllByNev(nev);
+    }
+
+    @GetMapping("/findfiltered")
+    public List<TermekEntity> findFiltered(@RequestParam(required = false) String meret,
+                                           @RequestParam(required = false) Float suly,
+                                           @RequestParam(required = false) Integer mennyiseg,
+                                           @RequestParam(required = false) String nev){
+
+        List<TermekEntity> termekek = repository.findAll();
+        return termekek.stream()
+                .filter(x -> nev == null || x.getNev().equals(nev))
+                .filter(x -> suly == null || x.getSuly().equals(suly))
+                .filter(x -> mennyiseg == null || x.getMennyiseg().equals(mennyiseg))
+                .filter(x -> meret == null || x.getMeret().equals(meret))
+                .toList();
+    }
+
+
 }
--- src/main/java/hu/unideb/inf/rendeles/data/repository/TermekRepository.java
+++ src/main/java/hu/unideb/inf/rendeles/data/repository/TermekRepository.java
@@ -4,6 +4,11 @@ import hu.unideb.inf.rendeles.data.entity.TermekEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TermekRepository extends JpaRepository<TermekEntity, Long> {
+
+    //select * from Termek where nev = ?nev
+    List<TermekEntity> findAllByNev(String nev);
 }
