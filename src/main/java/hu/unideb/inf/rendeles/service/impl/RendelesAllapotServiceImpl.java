f18f5206f0ffce68c75341f3f41c1ed97cc19c90
matesszabo <matesszabo@hotmail.com>
2024-11-04 13:23:23 +0100

    rendelesallapotserviceimpl

--- src/main/java/hu/unideb/inf/rendeles/service/impl/RendelesAllapotServiceImpl.java
+++ src/main/java/hu/unideb/inf/rendeles/service/impl/RendelesAllapotServiceImpl.java
@@ -1,20 +1,37 @@
 package hu.unideb.inf.rendeles.service.impl;
 
+import hu.unideb.inf.rendeles.data.entity.RendelesEntity;
+import hu.unideb.inf.rendeles.data.repository.RendelesRepository;
 import hu.unideb.inf.rendeles.service.RendelesAllapotService;
 import hu.unideb.inf.rendeles.service.dto.RendelesAllapotDto;
+import hu.unideb.inf.rendeles.service.mapper.RendelesAllapotMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class RendelesAllapotServiceImpl implements RendelesAllapotService {
+    @Autowired
+    RendelesAllapotMapper mapper;
+
+    @Autowired
+    RendelesRepository repo;
+
     @Override
     public RendelesAllapotDto getRendelesById(Long id) {
-        return null;
+        //return mapper.rendelesEntityToAllapotDto(repo.getReferenceById(id));
+
+        RendelesEntity entity = repo.getReferenceById(id);
+        RendelesAllapotDto dto = new RendelesAllapotDto();
+
+        dto = mapper.rendelesEntityToAllapotDto(entity);
+
+        return dto;
     }
 
     @Override
     public List<RendelesAllapotDto> getAllRendeles() {
-        return List.of();
+        return mapper.rendelesEntityToAllapotList(repo.findAll());
     }
 }
